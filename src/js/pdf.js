2eadcee906a599f0db02188374a90dc9b3a23253
Casey Webb <notcaseywebb@gmail.com>
2018-02-10 20:08:09 -0600

    Better PDF Scaling

--- src/js/pdf.js
+++ src/js/pdf.js
@@ -1,4 +1,36 @@
 async function renderPdfElement(el) {
+  await loadPDFJS()
+
+  const { PDFJS } = window
+  const url = el.getAttribute('data-src')
+  const container = createContainer(el)
+  const SCALE = 1.0
+
+  const pdfPage = await PDFJS.getDocument(url).then((d) => d.getPage(1))
+
+  const pdfPageView = new PDFJS.PDFPageView({
+    container,
+    id: 1,
+    scale: SCALE,
+    defaultViewport: pdfPage.getViewport(SCALE),
+    textLayerFactory: new PDFJS.DefaultTextLayerFactory()
+  })
+
+  pdfPageView.setPdfPage(pdfPage)
+
+  await pdfPageView.draw()
+}
+
+function renderPdfElements() {
+  for (const el of document.getElementsByTagName('pdf')) {
+    renderPdfElement(el).catch((err) => {
+      // eslint-disable-next-line no-console
+      console.error('Failed to render pdf', err)
+    })
+  }
+}
+
+async function loadPDFJS() {
   const styles = document.createElement('link')
   styles.setAttribute('rel', 'stylesheet')
   styles.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.0.347/pdf_viewer.css')
@@ -7,36 +39,17 @@ async function renderPdfElement(el) {
   await Promise.all([
     import('pdfjs-dist'),
     import('pdfjs-dist/web/pdf_viewer')
-  ])
-  
-  PDFJS.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js'
-  
-  const container = document.createElement('div')
-  const viewer = document.createElement('div')
-  const url = el.getAttribute('data-src')
-
-  viewer.classList.add('pdfViewer')
-  container.appendChild(viewer)
-  el.appendChild(container)
-
-  const pdfLinkService = new PDFJS.PDFLinkService()
-  
-  const pdfSinglePageViewer = new PDFJS.PDFSinglePageViewer({
-    container: container,
-    linkService: pdfLinkService
+  ]).then(() => {
+    window.PDFJS.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js'
   })
-
-  container.addEventListener('pagesinit', () => pdfSinglePageViewer.currentScaleValue = 'page-width')
-    
-  const pdfDocument = await PDFJS.getDocument(url)
-    
-  pdfLinkService.setViewer(pdfSinglePageViewer)
-  pdfSinglePageViewer.setDocument(pdfDocument)
-  pdfLinkService.setDocument(pdfDocument, null)
 }
 
-function renderPdfElements() {
-  for (const el of document.getElementsByTagName('pdf')) renderPdfElement(el)
+function createContainer(el) {
+  const container = document.createElement('div')
+  container.classList.add('pdfViewer')
+  container.classList.add('singlePageView')
+  el.appendChild(container)
+  return container
 }
 
 document.addEventListener('pjax:complete', renderPdfElements)
